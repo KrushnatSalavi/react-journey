513593304a0b13d6e1f23af69a706c4cb563e04f
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-17 20:07:46 +0530

    hooks in react

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,19 +3,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let counter = 15
 
-  const addValue = () =>{
-    counter = counter + 1  
+  let [counter,setCounter] = useState(15)
+ 
+  // let counter = 15
+
+  const addValue = () => {
+    // counter = counter + 1
+    setCounter(counter + 1)
   }
+
+  const removeValue = () =>{
+   
+    setCounter(counter - 1)
+  }
+
+
   return (
     <>
-    <h1>React </h1>
-    <h2>Counter value : {counter}</h2>
+      <h1>React </h1>
+      <h2>Counter value : {counter}</h2>
 
-    <button onClick={addValue}
-    >Add value</button><br/>
-     <button className='w-100px m-50px'>Remove value</button>
+      <button onClick={addValue}
+      >Add value</button><br />
+      <button onClick={removeValue}>Remove value</button>
 
     </>
   )
