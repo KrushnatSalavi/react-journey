61ede077a08d35b64e04a0fa5f5a2b90d894a8e7
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-18 16:13:44 +0530

    tailwind Css and Props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,7 +1,10 @@
+
+import Card from "./components/Card";
 function App() {
   return (
     <h1 className="bg-green-400 text-white p-6 text-4xl">
       Hello Tailwind
+      <Card/>
     </h1>
   );
 }
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-[500px] h-[400px] bg-amber-300'>
+      <h1>hii</h1>
+    </div>
+  )
+}
+
+export default Card
