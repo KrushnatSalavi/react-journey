a66717cdd0cc084efec7ca2ab644cf81750aab10
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-19 15:45:41 +0530

    tailwind Css and Props

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='w-[500px] h-[400px] bg-amber-300'>
-      <h1>hii</h1>
-    </div>
-  )
-}
+    <div className="relative w-[100px] h-[100px] rounded-xl overflow-hidden">
+  <img
+    src="/Images/rajgad.png"
+    alt="Rajgad"
+    width={'300px'} height={'auto'}
+/>
 
-export default Card
+  <div className="absolute inset-0 bg-black/40"></div>
+
+  <div className="absolute bottom-5 left-5 text-white">
+    <h1 className="text-3xl font-bold">Krushna Salavi</h1>
+    <p>Frontend Developer</p>
+  </div>
+</div>
+  );
+};
+
+export default Card;
+
