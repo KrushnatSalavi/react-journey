f23e20c2560c8cbc6e063d06fafcf5004cad52a8
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-07 18:52:30 +0530

    Password Generator project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setLength] = useState(8)
+  const [numberAllowed,setNumberAllowed] = useState(false);
+  const [charAllowed,setCharAllowed]= useState(false)
+  const [password,setPassword] = useState("")
 
   return (
    <>
