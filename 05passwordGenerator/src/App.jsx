1d686b15105444cce2623e138ededb32bb453f9d
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-07 20:27:29 +0530

    Password Generator project using useEffect, useRef and useCallback

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -2,19 +2,19 @@ import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [length,setLength] = useState(8)
-  const [numberAllowed,setNumberAllowed] = useState(false);
-  const [charAllowed,setCharAllowed]= useState(false)
-  const [password,setPassword] = useState("")
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
-  const passwordGenrator = useCallback(()=>{
-    let pass = '' 
+  const passwordGenrator = useCallback(() => {
+    let pass = ''
     let str = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
 
-    if(numberAllowed) str += '0123456789'
-    if(charAllowed) str += '!@#$%^&*()_{}|[]|-'
+    if (numberAllowed) str += '0123456789'
+    if (charAllowed) str += '!@#$%^&*()_{}|[]|-'
 
-    for(let i = 1; i<=Array.length; i++){
+    for (let i = 1; i <= Array.length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
 
       pass = str.charAt(char)
@@ -24,28 +24,33 @@ function App() {
 
 
 
-  }, [length,numberAllowed,charAllowed,setPassword])
+  }, [length, numberAllowed, charAllowed, setPassword])
 
 
   return (
-   <>
+    <>
 
-   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3
    my-8 bg-gray-800'>
 
-    <h1 className='text-white text-center'> Password Generator</h1>
+        <h1 className='text-white text-center'> Password Generator</h1>
 
-    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-      <input
-       type="text" 
-       value={password}
-       className='outline-none w-full py-1 px-3 bg-white'
-       placeholder='Password'
-       readOnly
-      />
-    </div>
-   </div>
-   </>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3 bg-white'
+            placeholder='Password'
+            readOnly
+          />
+          <button
+            className='outline-none bg-blue-700 text-white
+            px-3 py-0.5 shrink-0'>
+            Copy
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
