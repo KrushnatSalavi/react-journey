995d2a2e3d3d67d0ede4193409a67e101932dd13
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-07 20:22:03 +0530

    Password Generator project using useEffect, useRef and useCallback

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -30,9 +30,20 @@ function App() {
   return (
    <>
 
-   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 
-   my-8 text-orange-500 bg-gray-800'>
-    Test
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3
+   my-8 bg-gray-800'>
+
+    <h1 className='text-white text-center'> Password Generator</h1>
+
+    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+      <input
+       type="text" 
+       value={password}
+       className='outline-none w-full py-1 px-3 bg-white'
+       placeholder='Password'
+       readOnly
+      />
+    </div>
    </div>
    </>
   )
