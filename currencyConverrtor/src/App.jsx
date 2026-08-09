931583c416ebdddd51645f31b4e2cceb8cb01267
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-09 18:13:02 +0530

    Custom hooks in react  currency Project

--- currencyConverrtor/src/App.jsx
+++ currencyConverrtor/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   return (
     <>
-    
+      <div className='h-22 w-100 bg-gray-400 m-auto py-6 px-15 rounded-lg mt-30 '>
+          <h1>Hello </h1>
+      </div>
     </>
   )
 }
