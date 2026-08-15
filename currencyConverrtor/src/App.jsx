b3a088b65c712f01f30f86cf882d9dd2b7eea894
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-15 19:16:17 +0530

    useId hook

--- currencyConverrtor/src/App.jsx
+++ currencyConverrtor/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import {InputBox} from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <>
       <div className='h-22 w-100 bg-gray-400 m-auto py-6 px-15 rounded-lg mt-30 '>
           <h1>Hello </h1>
+          
       </div>
     </>
   )
