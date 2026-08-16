e939c3602817d678bda59f82791bfa5d804c40f4
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-16 08:57:55 +0530

    React router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -5,11 +5,9 @@ import './App.css'
 function App() {
 
   return (
-   <>
-   <h1 className='text-3xl font-bold underline text-red-500 text-center'>
-      Hello world!
-    </h1>
-   </>
+    <>
+      
+    </>
   )
 }
 
