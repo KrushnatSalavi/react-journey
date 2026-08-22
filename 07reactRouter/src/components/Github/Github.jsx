c60674f1c055a7af19014b3bac15974a5b25d308
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-22 19:56:36 +0530

    Github Api fetch

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Github = () => {
-useEffect( () =>{
-  fetch('http://api.github.com/users/KrushnatSalavi')
-  .then(response => response.json)
-}, [])
+  const [data, setdata] = useState([])
+
+  useEffect(() => {
+    fetch('http://api.github.com/users/KrushnatSalavi')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setdata(data)
+
+      })
+  }, [])
 
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-      Github Followers    
+      Github Followers :  {data.followers}
+
+      <img src={data.avatar_url} alt="Picture" width={200}  />
     </div>
   )
 }
